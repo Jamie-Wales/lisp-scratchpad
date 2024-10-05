2fae7813ca45131e06d96f5ff778d7bd62a2e89e
Jamie Wales <jamie.wales.developer@gmail.com>
2024-10-05 11:50:00 +0100

    I/O

--- database.lisp
+++ database.lisp
@@ -11,4 +11,21 @@
 
 (defun add-record (cd) (push cd *database*))
 
+(defun dump-db ()
+  (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
 
+(defun prompt-read(prompt)
+    (format *QUERY-IO* "~a:" prompt)
+    (force-output *QUERY-IO*)
+    (read-line *QUERY-IO*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+      (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
